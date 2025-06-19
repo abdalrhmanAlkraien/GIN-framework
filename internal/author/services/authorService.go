328aff6744c8318f16bcea83e8e4cd1673bbff13
Abdalrhman Alkraien <Abdalrhmanalkraien@gmail.com>
2025-06-19 08:36:37 +0300

    prepare project structuer

---
+++ internal/author/services/authorService.go
@@ -0,0 +1,18 @@
+package author_service
+
+import authorModel "web/internal/author/model"
+
+func CreateAuthor(author authorModel) {
+
+}
+
+func UpdateAuthor()
+
+func getAllAuthor() authorModel.Author {
+
+	return nil
+}
+
+func getAuthorById() authorModel.Author {
+
+}
